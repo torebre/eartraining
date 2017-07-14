c5a0454b67dd9f53e29331846a7d2cea7f6101a6
Tore Brede <tore.brede@gmail.com>
2017-07-14 16:22:43 +0200

    Small cleanup

--- noteGenerator/src/main/kotlin/com/kjipo/font/GlyphFactory.kt
+++ noteGenerator/src/main/kotlin/com/kjipo/font/GlyphFactory.kt
@@ -11,7 +11,6 @@ object GlyphFactory {
     val nameGlyphMap = mapOf(*loadGlyphs())
 
     val scale = 0.1
-    //    val glyphResource = "/gonville-r9313/lilyfonts/svg/emmentaler-11.svg"
     val glyphResource = "/glyphs.json"
     val blankGlyph = GlyphData("blank", emptyList(), 0, BoundingBox(0.0, 0.0, 0.0, 0.0))
 
@@ -27,18 +26,6 @@ object GlyphFactory {
     fun getGlyph(name: String): GlyphData {
         return nameGlyphMap.getOrDefault(name, blankGlyph)
     }
-
-//    private fun loadGlyphs(): Array<Pair<String, GlyphData>> {
-//        return javaClass.getResourceAsStream(glyphResource).use {
-//            ReadFonts.extractGlyphPaths(it).stream()
-//                    .map { glyphData -> scaleGlyph(glyphData, scale) }
-//                    .map { invertYCoordinates(it) }
-//                    .map { Pair(it.name, it) }
-//                    .toList()
-//                    .toTypedArray()
-//
-//        }
-//    }
 
     private fun loadGlyphs(): Array<Pair<String, GlyphData>> {
         val gson = Gson()
