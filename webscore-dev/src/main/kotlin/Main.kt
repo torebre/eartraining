24664dc1180d9c360a37db376f40cb4f8ac193e9
Tore Brede <tore.brede@gmail.com>
2019-11-14 21:00:09 +0100

    Adding synthesizer module as dependency from webscore-dev

--- webscore-dev/src/main/kotlin/Main.kt
+++ webscore-dev/src/main/kotlin/Main.kt
@@ -1,6 +1,7 @@
 import com.kjipo.handler.ScoreHandler
 import com.kjipo.score.*
 import com.kjipo.scoregenerator.SimpleSequenceGenerator
+import kotlin.browser.document
 import kotlin.browser.window
 
 
@@ -35,10 +36,49 @@ private fun generateTrainingSequence() {
 }
 
 
+private fun playNote() {
+
+    val samples: dynamic = Any()
+//        samples.C1 = "samples/UR1_C1_f_RR1.wav"
+//    samples.C1 = "chord.mp3"
+    samples.C1 = "UR1_C1_f_RR1.wav"
+    samples.C2 = "UR1_C2_f_RR1.wav"
+    samples.C3 = "UR1_C3_f_RR1.wav"
+    samples.C4 = "UR1_C4_f_RR1.wav"
+    samples.C5 = "UR1_C5_f_RR1.wav"
+    samples.C6 = "UR1_C6_f_RR1.wav"
+    samples.C7 = "UR1_C7_f_RR1.wav"
+//    samples.G2 = "UR1_G2_f_RR1.wav"
+//    samples.G3 = "UR1_G3_f_RR1.wav"
+//    samples.G4 = "UR1_G4_f_RR1.wav"
+//    samples.G5 = "UR1_G5_f_RR1.wav"
+//    samples.G6 = "UR1_G6_f_RR1.wav"
+//    samples.G7 = "UR1_G7_f_RR1.wav"
+
+    val parameters: dynamic = Any()
+    parameters.release = 1
+    parameters.baseUrl = "samples/"
+
+    var sampler = Tone.Sampler(samples, parameters)
+    sampler.toMaster()
+
+    Tone.Transport.start()
+
+    document.querySelector("button")!!.addEventListener("click", {
+
+        console.log("Test23")
+
+        playNote(sampler)
+    })
+}
+
+
 fun main() {
 //    setupEmptyBar()
 
     console.log("Test23")
+
+    playNote()
 
     setupTestScore2()
 
