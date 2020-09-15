d152514c8f134dab050d3c19bfc1d0292e080209
Tore Brede <tore.brede@gmail.com>
2020-09-15 21:41:17 +0200

    Use shorter enum names when serializing to JSON

--- score-multimodule/src/commonMain/kotlin/com/kjipo/svg/PathCommand.kt
+++ score-multimodule/src/commonMain/kotlin/com/kjipo/svg/PathCommand.kt
@@ -1,15 +1,37 @@
 package com.kjipo.svg
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 
+@Serializable
 enum class PathCommand constructor(val command: Char, val isAbsolute: Boolean) {
+    @SerialName("v")
     VERTICAL_LINE_TO_RELATIVE('v', false),
+
+    @SerialName("V")
     VERTICAL_LINE_TO_ABSOLUTE('V', true),
+
+    @SerialName("h")
     HORIZONAL_LINE_TO_RELATIVE('h', false),
+
+    @SerialName("M")
     MOVE_TO_ABSOLUTE('M', true),
+
+    @SerialName("m")
     MOVE_TO_RELATIVE('m', false),
+
+    @SerialName("l")
     LINE_TO_RELATIVE('l', false),
+
+    @SerialName("c")
     CURVE_TO_RELATIVE('c', false),
+
+    @SerialName("C")
     CURVE_TO_ABSOLUTE('C', true),
+
+    @SerialName("z")
     CLOSE_PATH('z', false),
-    SMOOTH_CURVE_TO_RELATIVE('s', false),
+
+    @SerialName("s")
+    SMOOTH_CURVE_TO_RELATIVE('s', false)
 }
