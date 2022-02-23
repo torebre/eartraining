82853f146d147712202936e51eb47c359486737d
Tore Brede <tore.brede@gmail.com>
2022-02-23 20:32:36 +0100

    Transferring function for concatenating files into Kotlin-code

--- webpitch2/src/main/kotlin/main.kt
+++ webpitch2/src/main/kotlin/main.kt
@@ -1,7 +1,13 @@
 import kotlinx.browser.document
 import kotlinx.browser.window
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.await
+import kotlinx.coroutines.launch
 import mu.KotlinLogging
 import org.w3c.dom.mediacapture.MediaStream
+import org.w3c.dom.url.URL
+import org.w3c.files.Blob
+import org.w3c.files.BlobPropertyBag
 import webaudioapi.AudioContext
 import webaudioapi.AudioNode
 import webaudioapi.MediaStreamAudioSourceNode
@@ -14,6 +20,7 @@ object MicrophoneInput {
 
     private var processData = false
     private var microphone: MediaStreamAudioSourceNode? = null
+
     // TODO It is an AudioWorkletNode, but where is the code for the interface?
     private var pitchNode: dynamic = Any()
     private val logger = KotlinLogging.logger {}
@@ -26,7 +33,11 @@ object MicrophoneInput {
 
         // Setting up a media stream from the microphone
         window.navigator.mediaDevices.getUserMedia(constraints)
-            .then { mediaStream -> processAudio(mediaStream) }
+            .then { mediaStream ->
+                GlobalScope.launch {
+                    processAudio(mediaStream)
+                }
+            }
             .catch { throwable ->
                 logger.error(throwable) {
                     "Exception when trying to process audio stream"
@@ -34,8 +45,26 @@ object MicrophoneInput {
             }
     }
 
+    suspend fun urlFromFiles(files: List<String>): String {
+        val promises = files
+            .map { file ->
+                window.fetch(file)
+                    .then { response ->
+                        response.text()
+                    }
+            }
 
-    fun processAudio(stream: MediaStream) {
+        val fileContents = Promise.all(promises.toTypedArray()).await()
+
+        return Promise.all(fileContents)
+            .then { stringPromises ->
+                val input = (arrayOf("var exports = {};") + stringPromises).joinToString("")
+                val blob = Blob(arrayOf(input), BlobPropertyBag("application/javascript"))
+                URL.createObjectURL(blob)
+            }.await()
+    }
+
+    suspend fun processAudio(stream: MediaStream) {
         if (stream.active) {
             if (audioContext.state == "closed") {
                 logger.error { "AudioContext state is closed" }
@@ -48,40 +77,42 @@ object MicrophoneInput {
                 js("RingBuffer.getStorageForCapacity(3, Float32Array);") // capacity: three float32 values [pitch, confidence, rms]
             val rb = js("new RingBuffer(sharedArrayBuffer, Float32Array);")
             val audioReader = js("new AudioReader(rb);")
+            val fileContents = urlFromFiles(
+                listOf(
+                    "essentia-wasm.umd.js",
+                    "essentia.js-core.umd.js",
+                    "pitchyinprob-processor.js",
+                    "index.js"
+                )
+            )
 
-            val loadEssentiaFilesPromise: Promise<Any> =
-                js("URLFromFiles([\"essentia-wasm.umd.js\", \"essentia.js-core.umd.js\", \"pitchyinprob-processor.js\", \"index.js\"])")
+            val addModulePromise: Promise<Any> =
+                js("webpitch2.MicrophoneInput.audioContext.audioWorklet.addModule(fileContents);")
 
-            loadEssentiaFilesPromise.then { code ->
-                val addModulePromise: Promise<Any> =
-                    js("webpitch2.MicrophoneInput.audioContext.audioWorklet.addModule(code);")
+            addModulePromise.then {
+                pitchNode =
+                    js("new AudioWorkletNode(webpitch2.MicrophoneInput.audioContext, 'pitchyinprob-processor', { processorOptions: { bufferSize: 8192, sampleRate: webpitch2.MicrophoneInput.audioContext.sampleRate}});")
 
-                addModulePromise.then {
-                    pitchNode =
-                        js("new AudioWorkletNode(webpitch2.MicrophoneInput.audioContext, 'pitchyinprob-processor', { processorOptions: { bufferSize: 8192, sampleRate: webpitch2.MicrophoneInput.audioContext.sampleRate}});")
+                val input: dynamic = Any()
+                input.sab = sharedArrayBuffer
 
-                    val input: dynamic = Any()
-                    input.sab = sharedArrayBuffer
+                // Set up the audio network
+                pitchNode.port.postMessage(input)
+                val gain = audioContext.createGain()
 
-                    // Set up the audio network
-                    pitchNode.port.postMessage(input)
-                    val gain = audioContext.createGain()
-
-                    // TODO Probably need to create a js-file with the pitch detection Essentia code that can be loaded here
+                // TODO Probably need to create a js-file with the pitch detection Essentia code that can be loaded here
 //                    val pitchDetection = PitchDetection
 //                    js("registerProcessor(\"pitch-detection-processor\", pitchDetection);")
 //                    val pitchNode =
 //                        js("new AudioWorkletNode(webpitch2.MicrophoneInput.audioContext, 'pitchyinprob-processor', { processorOptions: { bufferSize: 8192, sampleRate: webpitch2.MicrophoneInput.audioContext.sampleRate}});")
 
-                    gain.gain.setValueAtTime(0, audioContext.currentTime)
+                gain.gain.setValueAtTime(0, audioContext.currentTime)
 
-                    microphone?.connect(pitchNode as AudioNode)
-                    pitchNode?.connect(gain)
-                    gain.connect(audioContext.destination)
-                }
-                    .catch({ throwable -> logger.info(throwable, { "Exception when setting up audio graph" }) })
+                microphone?.connect(pitchNode as AudioNode)
+                pitchNode?.connect(gain)
+                gain.connect(audioContext.destination)
             }
-                .catch({ throwable -> logger.info(throwable, { "Exception when loading module" }) })
+                .catch({ throwable -> logger.info(throwable, { "Exception when setting up audio graph" }) })
 
             val pitchBuffer = js("new Float32Array(3);")
 
@@ -119,12 +150,9 @@ object MicrophoneInput {
         processData = false
 
 
-
-
     }
 
 }
-
 
 fun main() {
     document.write("Starting")
