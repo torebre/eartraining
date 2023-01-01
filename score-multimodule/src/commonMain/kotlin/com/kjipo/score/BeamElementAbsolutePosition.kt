66e0c60bb1bac7e5cf1604a1c92d139e81b8b945
Tore Brede <tore.brede@gmail.com>
2023-01-01 14:38:22 +0100

    Fixing issue with beam square calculation

--- score-multimodule/src/commonMain/kotlin/com/kjipo/score/BeamElementAbsolutePosition.kt
+++ score-multimodule/src/commonMain/kotlin/com/kjipo/score/BeamElementAbsolutePosition.kt
@@ -8,13 +8,13 @@ import com.kjipo.svg.findBoundingBox
  */
 class BeamElementAbsolutePosition(
     val id: String,
-    private val lowerLeftCoordinates: Pair<Double, Double>,
+    private val upperLeftCoordinates: Pair<Double, Double>,
     private val upperRightCoordinates: Pair<Double, Double>
 ) : ScoreRenderingElement() {
 
     override fun toRenderingElement(): List<PositionedRenderingElement> {
         val beamElement = addBeam(
-            lowerLeftCoordinates.first, lowerLeftCoordinates.second,
+            upperLeftCoordinates.first, upperLeftCoordinates.second - DEFAULT_BEAM_HEIGHT,
             upperRightCoordinates.first, upperRightCoordinates.second
         )
 
