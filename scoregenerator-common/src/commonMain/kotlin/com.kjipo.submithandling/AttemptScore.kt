861a53c419c1f303b2dfd62d18c9d7a64591cb74
Tore Brede <tore.brede@gmail.com>
2023-01-02 13:45:53 +0100

    Beginnings of class to score an attempt at practice example

---
+++ scoregenerator-common/src/commonMain/kotlin/com.kjipo.submithandling/AttemptScore.kt
@@ -0,0 +1,15 @@
+package com.kjipo.scoregenerator
+
+import com.kjipo.score.NoteSequenceElement
+
+class AttemptScorer {
+
+
+    fun scoreAttempt(target: List<NoteSequenceElement>, attempt: List<NoteSequenceElement>) {
+        // TODO
+
+
+    }
+
+
+}
